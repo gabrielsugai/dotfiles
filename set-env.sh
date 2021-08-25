b33b9ec3e7d6c463ec696f2d131f060be7473759
Gabriel Sugai <gabrielsugai@gmail.com>
2021-08-25 18:06:05 -0300

    Executa tmux somente se nao existir nenhum aberto

--- set-env.sh
+++ set-env.sh
@@ -1,21 +1,34 @@
 #!/bin/sh
 
-session="development"
+if [ -z "$TMUX" ]; then
+	session="development"
 
-# set up tmux
-tmux start-server
+	# set up tmux
+	tmux start-server
+	# create a new tmux session, starting vim from a saved session in the new window
 
-# create a new tmux session, starting vim from a saved session in the new window
-tmux new-session -d -s $session -n dev-env 
-tmux send-keys "iugu;dev-env" C-m
+	tmux new-session -d -s $session -n dev-env 
+	tmux send-keys "iugu;dev-env" C-m
 
-tmux new-window -t $session:1 -n platform 
-tmux send-keys "iugu;platform" C-m
+	tmux new-window -t $session:1 -n platform 
+	tmux send-keys "iugu;platform" C-m
 
-tmux new-window -t $session:2 -n develop 
+	tmux new-window -t $session:2 -n vault
+	tmux send-keys "iugu;smart-vault" C-m
 
-# return to main vim window
-tmux select-window -t $session:0
+	tmux new-window -t $session:3 -n pixugu
+	tmux send-keys "iugu;pixugu" C-m
 
-# Finished setup, attach to the tmux session!
-tmux attach-session -t $session
+	tmux new-window -t $session:4 -n boletos
+	tmux send-keys "iugu;registra-boletos" C-m
+
+	tmux new-window -t $session:5 -n develop 
+
+	# return to main vim window
+	tmux select-window -t $session:0
+
+	# Finished setup, attach to the tmux session!
+	tmux attach-session -t $session
+else
+	echo "already in tmux!"
+fi
