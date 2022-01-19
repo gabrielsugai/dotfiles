46012f1a00326a37a1fad1ce36d3c6dab92ce1b8
Gabriel Sugai <gabrielsugai@gmail.com>
2022-01-19 17:44:35 -0300

    Adiciona script para multiplos paineis no tmux

--- set-env.sh
+++ set-env.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+################################################################################
+###########################  Single Panel Script  ##############################
+################################################################################
+
 if [ -z "$TMUX" ]; then
 	session="development"
 
@@ -22,3 +26,43 @@ if [ -z "$TMUX" ]; then
 else
 	echo "already in tmux!"
 fi
+
+################################################################################
+###########################  Multi Panels Script  ##############################
+################################################################################
+
+# if [ -z "$TMUX" ]; then
+# 	session="development"
+#		# set your projects dir, exemplo: "$HOME/projects"
+# 	dir="$HOME"
+# 	# panels name
+# 	panels=( develop )
+#
+# 	#project dirs
+# 	projects=( develop )
+#
+# 	# set up tmux
+# 	tmux start-server
+#
+# 	# create a new tmux session, and create all panels
+# 	for i in ${!projects[@]}; do
+# 		if [ $i = 0 ]; then
+# 			tmux new-session -d -s $session -n ${panels[$i]} -c "$dir/${projects[$i]}"
+# 			tmux split-window -h -c "$dir/${projects[$i]}"
+# 		else
+# 			tmux new-window -n ${panels[$i]} -c "$dir/${projects[$i]}"
+# 			tmux split-window -v -c "$dir/${projects[$i]}"
+# 		fi
+# 	done
+#
+# 	tmux new-window -n develop
+# 	tmux split-window -v
+#
+# 	# return to main vim window
+# 	tmux select-window -t $session:1
+#
+# 	# Finished setup, attach to the tmux session!
+# 	tmux attach-session -t $session
+# else
+# 	echo "already in tmux!"
+# fi
